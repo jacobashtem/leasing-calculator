d0be9074c2bd7b20b147e33aeecd43bd5897d139
Jacob Ashtem <jacob.ashtem@gmail.com>
2024-05-14 12:27:03 +0200

    main nav & page header added, custom font, global css setup

---
+++ components/MainNav.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="sticky top-0 bg-white shadow-lg z-50">
+        <div class="w-full">
+            <div class="mx-auto container min-w-80 transition-[max-width]">
+                <div class="w-full bg-white">
+                    <div class="flex justify-between items-center min-h-14 md:min-h-[70px]">
+                        <NuxtLink to="/">
+                            <svg width="253" height="40" viewBox="0 0 253 40" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-32 md:w-auto"><path d="M71.586 27.7182L80.2113 0.554688H88.2135L74.7413 39.4464H68.0115L54.5889 0.554688H62.995L71.586 27.7182Z" fill="black"></path><path d="M104.023 0.554688H129.46V7.29981H111.742V16.477H126.534V23.2049H111.742V32.7013H129.46V39.4464H104.023V0.554688Z" fill="black"></path><path d="M147.259 0.554688H155.044V16.4436H170.95V0.554688H178.752V39.4464H170.95V23.2049H155.044V39.4464H147.259V0.554688Z" fill="black"></path><path d="M237.923 33.0709C241.632 33.0709 244.567 31.6948 244.567 28.5739C244.567 20.6042 224.821 25.9561 224.821 11.9131C224.821 4.0606 230.944 0 238.444 0C243.309 0 248.21 1.72866 251.816 4.58091L248.426 9.83183C245.473 7.81821 241.867 6.76226 238.88 6.76226C235.29 6.76226 232.487 8.30516 232.487 11.3089C232.487 19.2624 252.403 13.5741 252.403 27.9706C252.403 36.0566 246.128 40 238.292 40C232.587 40 227.689 37.8863 223.695 34.631L227.136 29.2295C230.591 31.7454 234.3 33.0709 237.923 33.0709Z" fill="black"></path><path d="M206.515 0.554688H198.711V39.4464H206.515V0.554688Z" fill="black"></path><path d="M39.9991 0H0V39.9991H39.9991V0Z" fill="#473A4E"></path><path d="M39.9997 5.31641C20.8753 5.31641 5.31641 20.8753 5.31641 39.9997H40.0006L39.9997 5.31641Z" fill="url(#vehis_logo)"></path><defs><linearGradient id="vehis_logo" x1="45.0379" y1="10.0207" x2="10.8574" y2="45.043" gradientUnits="userSpaceOnUse"><stop stop-color="#DB1A21"></stop><stop offset="0.5277" stop-color="#5E0000"></stop><stop offset="1" stop-color="#483A4F"></stop></linearGradient></defs></svg>
+                        </NuxtLink>
+                        <div>
+                            <div class="gap-3 items-center hidden md:flex">
+                                <div class="text-base text-dark-gray flex items-center gap-1">
+                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.98816 2.98816C2.67559 3.30072 2.5 3.72464 2.5 4.16667V5C2.5 11.9033 8.09667 17.5 15 17.5H15.8333C16.2754 17.5 16.6993 17.3244 17.0118 17.0118C17.3244 16.6993 17.5 16.2754 17.5 15.8333V13.1008C17.5 12.9259 17.445 12.7553 17.3427 12.6134C17.2404 12.4714 17.096 12.3653 16.93 12.31L13.1858 11.0617C12.9956 10.9984 12.7888 11.0059 12.6036 11.0827C12.4184 11.1596 12.2671 11.3006 12.1775 11.48L11.2358 13.3608C9.19538 12.4389 7.5611 10.8046 6.63917 8.76417L8.52 7.8225C8.69938 7.73288 8.84042 7.58158 8.91726 7.39637C8.9941 7.21116 9.00158 7.00445 8.93833 6.81417L7.69 3.07C7.63475 2.90413 7.52874 2.75984 7.38696 2.65754C7.24519 2.55525 7.07483 2.50013 6.9 2.5H4.16667C3.72464 2.5 3.30072 2.67559 2.98816 2.98816Z" stroke="#473A4E" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                                    <a href="tel:+48221033974">+48 22 103 39 74</a>
+                                </div>
+                                <div class="text-xs text-gray-400 font-extralight">9:00 - 21:00</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ components/PageHeader.vue
@@ -0,0 +1,5 @@
+<template>
+    <section class="px-4 py-6 sm:px-8">
+        <h2 class="text-2xl py-6 text-center"> Obliczanie szacunkowej skladki rocznej ubezpieczenia OC/AC na podstawie wieku samochodu i wartosci auta </h2>
+    </section>
+</template>
